c75ade346a3474bff31388d9406123c9c1a47cf0
Tom Ardern <tom.ardern@gmail.com>
2013-03-12 18:36:21 +0000

    Added Dat.Gui

--- julia.js
+++ julia.js
@@ -2,63 +2,87 @@
 
 function julia(attr){
 
-		data = {};
-		var px, py, it;
-		var c1, c2, c3, m;
+	var start = attr.start;
+	var end = attr.end;
+	var w = h = attr.size;
+	var zoom = 1;
+	var vx = 0;			//Viewpoints X
+	var vy = 0;			//Viewpoint Y
 
-		cr = -0.835;
-		ci = -0.2321;
-
-		for(i = attr.start; i < attr.end; i ++) {
-			var offset = i*4;
-
-			px = (i % attr.w );
-			py = attr.h - ((i/attr.w) | 0);
+	/* Julia Set Items  ------------------------ */
+	var cr = -0.835;
+	var ci = -0.2321;
 
 
-			x = attr.xmin + px/attr.w * attr.dx;
-			y = attr.ymin + py/attr.h * attr.dy;
-			it = attr.mi;
 
-			//Julia Code
-			var xx = x*x, yy = y*y;
-			do {
+
+	var mi = Math.max(30, Math.round(Math.max(Math.log(zoom),1) * 30)); //30 is startit
+	var itfac = 1/mi*30;
+
+	var xmin = vx - 2/zoom;
+	var xmax = vx + 2/zoom;
+	var ymin = vy - 2/zoom;
+	var ymax = vy + 2/zoom;
+
+	var dx = xmax-xmin;
+	var dy = ymax-ymin;
+
+
+	var data = {};
+	var px, py, it;
+	var c1, c2, c3, m;
+
+
+
+	for(i = start; i < end; i ++) {
+		var offset = i*4;
+
+		px = (i % w );
+		py = h - ((i/w) | 0);
+
+		x = xmin + px/w * dx;
+		y = ymin + py/h * dy;
+		it = mi;
+
+		//Julia Code
+		var xx = x*x, yy = y*y;
+		do {
+			y = (x+x)*y + ci;
+			x = xx - yy + cr;
+			yy = y*y;
+			xx = x*x;
+
+			if (xx+yy > 4) {
 				y = (x+x)*y + ci;
 				x = xx - yy + cr;
 				yy = y*y;
 				xx = x*x;
 
-				if (xx+yy > 4) {
-					y = (x+x)*y + ci;
-					x = xx - yy + cr;
-					yy = y*y;
-					xx = x*x;
+				break;
+			}
+		} while(--it);
+		res = {
+			it : it, x : x, y : y, xx : xx, yy : yy
+		};
 
-					break;
-				}
-			} while(--it);
-			res = {
-				it : it, x : x, y : y, xx : xx, yy : yy
-			};
+		//Julia Code here
+		it = mi - res.it;
 
-			//Julia Code here
-			it = attr.mi - res.it;
+		var mag =  it * itfac;
 
-			var mag =  it * attr.itfac;
+		c1 = c2 = c3 = (res.it==mi) ? 255 : 255 - mag/30 * 255;
+		c1 = c1 < 0 ? 0 : c1;
+		c2 = c2 < 0 ? 0 : c2;
+		c3 = c3 < 0 ? 0 : c3;
 
-			c1 = c2 = c3 = (res.it==attr.mi) ? 255 : 255 - mag/30 * 255;
-			c1 = c1 < 0 ? 0 : c1;
-			c2 = c2 < 0 ? 0 : c2;
-			c3 = c3 < 0 ? 0 : c3;
+		c1 = c1 > 255 ? 255 : c1;
+		c2 = c2 > 255 ? 255 : c2;
+		c3 = c3 > 255 ? 255 : c3;
 
-			c1 = c1 > 255 ? 255 : c1;
-			c2 = c2 > 255 ? 255 : c2;
-			c3 = c3 > 255 ? 255 : c3;
+		data[offset] = c1;
+		data[offset+1] = c2;
+		data[offset+2] = c3;
+	}
 
-			data[offset] = c1;
-			data[offset+1] = c2;
-			data[offset+2] = c3;
-		}
-
-	return {core: attr.core, data: data};
+	return {data: data};
 }
